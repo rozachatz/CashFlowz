e194d08bddac74c24d2a10f922bcc7625de4e389
rozachatz <rozaxatz@gmail.com>
2024-09-03 14:49:35 +0300

    fix: rm events from previous commit

--- src/main/java/com/moneytransfer/idempotent/event/IdempotentTransferRequestEvent.java
+++
@@ -1,9 +0,0 @@
-package com.moneytransfer.idempotent.event;
-
-import org.springframework.context.ApplicationEvent;
-
-public abstract class IdempotentTransferRequestEvent extends ApplicationEvent {
-    public IdempotentTransferRequestEvent(Object source) {
-        super(source);
-    }
-}
--- src/main/java/com/moneytransfer/idempotent/eventpublisher/EventPublisher.java
+++
@@ -1,8 +0,0 @@
-package com.moneytransfer.idempotent.eventpublisher;
-
-import jakarta.validation.constraints.NotNull;
-
-public interface EventPublisher <T> {
-        void publishEvent(@NotNull T applicationEvent);
-}
-
